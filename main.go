5d28680b42dbedf673a8b118fb8fd768f3294129
inheritancezx <fayzaaqilaar@gmail.com>
2024-12-04 13:56:25 +0700

    feat: ui changes and package get

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 
 func main() {
 	config.ConnectDB()
+	http.Handle("/img/", http.StripPrefix("/img/", http.FileServer(http.Dir("./img"))))
 
 	// 1.Homepage
 	http.HandleFunc("/", homecontroller.Welcome)
